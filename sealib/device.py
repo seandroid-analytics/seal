4b14ba57f30a6d43b9b58cf432455ad31bacc2bf
Filippo Bonazzi <filippo.bonazzi@aalto.fi>
2015-12-18 12:56:04 +0200

    Added module docstring

--- sealib/device.py
+++ sealib/device.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-"""TODO: file docstring"""
+"""Utility module to handle an Android device."""
 
 import subprocess
 import logging
